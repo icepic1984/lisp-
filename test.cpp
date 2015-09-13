368e1e081331d5e25407bacdd1b388d5d8e4e7b7
icepic <icepic2009@googlemail.com>
2015-09-13 14:38:06 +0200

    Add test cases for cons

---
+++ test.cpp
@@ -0,0 +1,57 @@
+#define BOOST_TEST_MODULE lisp_command
+
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+#include "parser.hpp"
+#include "sexpr.hpp"
+#include "lisp.hpp"
+#include "environment.hpp"
+
+//http://www.schemers.org/Documents/Standards/R5RS/HTML/
+BOOST_AUTO_TEST_CASE(cons_test)
+{
+	// (cons 'a '()) => (a)
+	auto env = std::make_unique<environment>();
+	auto expr = evals(parse(tokenize("(cons (quote a) (quote()))")),env.get());
+	auto l = expr.get<std::vector<sexpr>>();
+	BOOST_CHECK_EQUAL(l.size(),1);
+	std::stringstream buffer;
+	buffer << expr;
+	BOOST_CHECK_EQUAL(buffer.str(),"( a ) ");
+
+	// (cons '(a) '(b c d)) => ((a) b c d)
+	expr = evals(parse(tokenize("(cons (quote(a))  (quote(b c d))) ")),env.get());
+	l = expr.get<std::vector<sexpr>>();
+	BOOST_CHECK_EQUAL(l.size(),4);
+	buffer.str(std::string());
+	buffer.clear();
+	buffer << expr;
+	BOOST_CHECK_EQUAL(buffer.str(),"( ( a ) b c d ) ");
+
+	// (cons "a" '(b c))
+	expr = evals(parse(tokenize("(cons \"a\"  (quote(b c))) ")),env.get());
+	buffer.str(std::string());
+	buffer.clear();
+	buffer << expr;
+	BOOST_CHECK_EQUAL(buffer.str(),"( \"a\" b c ) ");
+
+	// (cons 'a 3)
+	expr = evals(parse(tokenize("(cons (quote a) 3)")),env.get());
+	buffer.str(std::string());
+	buffer.clear();
+	buffer << expr;
+	BOOST_CHECK_EQUAL(buffer.str(),"( a 3 ) ");
+
+	// (cons '(a b) 'c)
+	expr = evals(parse(tokenize("(cons (quote(a b)) (quote c))")),env.get());
+	buffer.str(std::string());
+	buffer.clear();
+	buffer << expr;
+	BOOST_CHECK_EQUAL(buffer.str(),"( ( a b ) c ) ");
+}
+
+
+
+// (cons 'a '(b c))
+// (cons '(a) '(b c))
+// (cons '(b c) 'a)
